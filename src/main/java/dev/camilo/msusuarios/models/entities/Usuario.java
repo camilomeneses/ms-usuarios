1c63eaba90124461b2fc899cb5a0679a991dc900
Camilo Meneses <kamilomeneses@gmail.com>
2023-02-11 22:51:27 -0500

    MS-USUARIOS: primer commit

---
+++ src/main/java/dev/camilo/msusuarios/models/entities/Usuario.java
@@ -0,0 +1,4 @@
+package dev.camilo.msusuarios.models.entities;
+
+public class Usuario {
+}
---
+++ src/test/java/dev/camilo/msusuarios/MsUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.camilo.msusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
