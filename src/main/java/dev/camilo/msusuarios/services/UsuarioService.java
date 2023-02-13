530de3d8317aa17aeecf266f752f26bab269f1a5
Camilo Meneses <kamilomeneses@gmail.com>
2023-02-12 22:49:26 -0500

    MS-USUARIOS: add metodos HTTP Feign para comunicacion con ms-cursos

--- src/main/java/dev/camilo/msusuarios/services/UsuarioService.java
+++ src/main/java/dev/camilo/msusuarios/services/UsuarioService.java
@@ -13,4 +13,9 @@ public interface UsuarioService {
   void eliminar(Long id);
   Optional<Usuario> porEmail(String email);
   boolean existePorEmail(String email);
+
+  // Para comunicacion Feign con ms-cursos
+  Optional<Usuario> asignarUsuario(Usuario usuario,Long cursoId);
+  Optional<Usuario> crearUsuario(Usuario usuario, Long cursoId);
+  Optional<Usuario> desasignarUsuario(Usuario usuario, Long cursoId);
 }
