19fa25cb451535d23dc332f033786dc811c0e638
Camilo Meneses <kamilomeneses@gmail.com>
2023-04-07 11:15:47 -0500

    MS-USUARIOS: config ms con spring cloud k8s

--- src/main/java/dev/camilo/msusuarios/MsUsuariosApplication.java
+++ src/main/java/dev/camilo/msusuarios/MsUsuariosApplication.java
@@ -3,11 +3,13 @@ package dev.camilo.msusuarios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class MsUsuariosApplication {
 
