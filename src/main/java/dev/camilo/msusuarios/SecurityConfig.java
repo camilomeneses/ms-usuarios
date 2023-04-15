37cf01c2b7451d6c827e33d2274d3716acfbac8d
Camilo Meneses <kamilomeneses@gmail.com>
2023-04-14 20:37:46 -0500

    MS-USUARIOS: configurando oauth2 resource server

--- src/main/java/dev/camilo/msusuarios/SecurityConfig.java
+++ src/main/java/dev/camilo/msusuarios/SecurityConfig.java
@@ -1,6 +1,7 @@
 package dev.camilo.msusuarios;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,12 +16,19 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-    http.authorizeRequests().anyRequest().authenticated()
+    http.authorizeRequests()
+        .requestMatchers("/authorized").permitAll()
+        .requestMatchers(HttpMethod.GET, "/").hasAnyAuthority("SCOPE_read","SCOPE_write")
+        .requestMatchers(HttpMethod.POST, "/").hasAuthority("SCOPE_write")
+        .requestMatchers(HttpMethod.PUT, "/{id}").hasAuthority("SCOPE_write")
+        .requestMatchers(HttpMethod.DELETE, "/{id}").hasAuthority("SCOPE_write")
+        .anyRequest().authenticated()
         .and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
         .oauth2Login(oauth2Login -> oauth2Login.loginPage("/oauth2/authorization/ms-usuarios-client"))
-        .oauth2Client(withDefaults());
+        .oauth2Client(withDefaults())
+        .oauth2ResourceServer().jwt();
 
     return http.build();
   }
